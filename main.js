f42675623c18b57b8881452eafbcd1600a942f24
Shell-thon <shellterminal0@gmail.com>
2023-07-03 03:11:56 +0100

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import 'boxicons'
+import Typed from 'typed.js';
 var typed=new typed (".text", {
     String:["innovative 3D game", "built using raycasting technology", "SDL2 for this project"],
     typedSpeed:100,
